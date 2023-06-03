0f66fc280f0721e4620aacc647d2362a45d09768
JakobGlieder <58510986+JakobGlieder@users.noreply.github.com>
2023-06-03 23:54:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('update-btn').addEventListener('click', function() {
+    window.open('mailto:Jakob@companynamecomingsoon.com?subject=What\'s coming soon?&body=Keep me updated');
+});
